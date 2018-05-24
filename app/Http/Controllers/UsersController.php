b9948564ca4a3e014ffc066cc645056755485aed
yage yang <jacob_yang@yeah.net>
2018-05-24 01:47:22 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -18,7 +19,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-     public function store(Request $request)
+    public function store(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|max:50',
@@ -32,6 +33,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
